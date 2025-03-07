f20526eaef76afdcf8aeddcdd961bf37e626046d
Romano Romano <romano.romano@gmail.com>
2025-03-07 15:16:31 +0100

    feat: add blueraven.it zone

---
+++ blueraven.it.tf
@@ -0,0 +1,14 @@
+# Zone
+resource "cloudflare_zone" "blueraven_it" {
+
+  account = {
+    id = cloudflare_account.this.id
+  }
+
+  name = "blueraven.it"
+}
+
+# Outputs
+output "blueraven_it_zone_id" {
+  value = cloudflare_zone.blueraven_it.id
+}
