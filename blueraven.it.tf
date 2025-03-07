fd8aaae5d354db5dbf1b1f64a7cdf761ed98157d
Romano Romano <romano.romano@gmail.com>
2025-03-07 15:41:26 +0100

    feat: add name servers output

--- blueraven.it.tf
+++ blueraven.it.tf
@@ -9,6 +9,10 @@ resource "cloudflare_zone" "blueraven_it" {
 }
 
 # Outputs
+output "blueraven_it_name_servers" {
+  value = cloudflare_zone.blueraven_it.name_servers
+}
+
 output "blueraven_it_zone_id" {
   value = cloudflare_zone.blueraven_it.id
 }
